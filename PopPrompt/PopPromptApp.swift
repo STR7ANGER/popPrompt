7dde942cbc0f5afa881303c25beac5f96024809d
STR7ANGER <adityamaurya.2807@gmail.com>
2026-04-19 13:32:59 +0530

    Fix app launch and DMG packaging

--- PopPrompt/PopPromptApp.swift
+++ PopPrompt/PopPromptApp.swift
@@ -5,6 +5,12 @@ struct PopPromptApp: App {
     @StateObject private var store = PromptStore()
 
     var body: some Scene {
+        WindowGroup("PopPrompt") {
+            ContentView()
+                .environmentObject(store)
+        }
+        .defaultSize(width: 420, height: 520)
+
         MenuBarExtra("PopPrompt", systemImage: "text.bubble") {
             ContentView()
                 .environmentObject(store)
